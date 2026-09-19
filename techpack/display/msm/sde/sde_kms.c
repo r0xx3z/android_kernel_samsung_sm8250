647de878810402f60ad56cfd77c89bbb6646afb6
r0xx3z <axelgpg2290@gmail.com>
2026-09-19 17:52:29 -0500

    drm/msm/sde: fix undeclared 'ret' in sde_kms_disable_vblank

--- techpack/display/msm/sde/sde_kms.c
+++ techpack/display/msm/sde/sde_kms.c
@@ -303,9 +303,7 @@ static int sde_kms_enable_vblank(struct msm_kms *kms, struct drm_crtc *crtc)
 
 static void sde_kms_disable_vblank(struct msm_kms *kms, struct drm_crtc *crtc)
 {
-#if defined(CONFIG_DISPLAY_SAMSUNG) // case 04436106
 	int ret = 0;
-#endif
 
 	SDE_ATRACE_BEGIN("sde_kms_disable_vblank");
 	ret = sde_crtc_vblank(crtc, false);
